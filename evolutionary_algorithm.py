bdcb270b2f5cbf246f0182591946abbe56cca142
Kornel <kornelzaba@gmail.com>
2018-06-07 11:16:24 +0200

    Fixed some errors in the algorithm

--- evolutionary_algorithm.py
+++ evolutionary_algorithm.py
@@ -18,19 +18,28 @@ def random_search(fun, lbounds, ubounds, budget):
     population_limit = 200
     tournament_size = 15
     tournament_number = 10
-    winner_count = 2
+    winner_count = 4
     loser_count = 2
-    sigma = 0.2
+    sigma = 1.0
     mu = 0.0
     local_optimizer_num = 1
 
+    log = open("log.txt", 'a+')
+    log.write("Budget {}\n".format(budget))
+    log.write("Function {}\n".format(fun))
+    log.write("Population size {}, population limit {}\n".format(population_size, population_limit))
+    log.write("Tournament size {}, tournament number {}\n".format(tournament_size, tournament_number))
+    log.write("Winner_count {}, loser count {}\n".format(winner_count, loser_count))
+    log.write("Mu {}, sigma {}, optimization iters {}\n".format(mu, sigma, local_optimizer_num))
+
     lbounds, ubounds = np.array(lbounds), np.array(ubounds)
-    dim, x_min, f_min = len(lbounds), None, None
+    dim, i_min, x_min, f_min = len(lbounds), None, None, None
     population_number = 0
     X = lbounds + (ubounds - lbounds) * generate_population(population_size, dim)
+
     while budget > 0 and population_number < population_limit:
         for i in range(tournament_number):
-            if budget <= 0:
+            if budget <= 0 or fun.final_target_hit == 1:
                 break
             T = []
             I = []
@@ -40,19 +49,24 @@ def random_search(fun, lbounds, ubounds, budget):
                 T.append(X[row, :])
             T = np.asarray(T)
             F = np.apply_along_axis(fun, 1, T)
-            budget -= dim * tournament_size
-            if budget <= 0:
+            budget -= tournament_size
+            if budget <= 0 or fun.final_target_hit == 1:
                 break
             I_and_F = sorted(zip(I, F), key=lambda x: x[1])
-            x_min = I_and_F[0][0]
+            x_min = X[I_and_F[0][0]]
             normal_dist = np.random.normal(mu, sigma, (winner_count, dim))
             for p in range(winner_count):
-                X[I_and_F[p][0], :] = X[I_and_F[p][0], :] + normal_dist[p, :]
+                X[I_and_F[p][0], :] = np.clip(X[I_and_F[p][0], :] + normal_dist[p, :], lbounds, ubounds)
             I_and_F = list(reversed(I_and_F))
             for p in range(loser_count):
                 res = scipy.optimize.minimize(fun, X[I_and_F[p][0], :], args=(), method='BFGS', jac=None, tol=None, callback=None,
-                                              options={'disp': False, 'maxiter': local_optimizer_num})
-                X[I_and_F[p][0], :] = res.x
-                budget -= res.nfev * dim
+                                              options={'disp': False, 'maxiter': local_optimizer_num, 'maxfun': 1})
+                X[I_and_F[p][0], :] = np.clip(res.x, lbounds, ubounds)
+                budget -= res.nfev
         population_number = population_number + 1
+        if fun.final_target_hit == 1:
+            break
+
+    log.write("Best target reached {}\n".format(fun.final_target_hit))
+    log.close()
     return x_min
